7dc8cb0073397c54510a82f52b7210360a9d3716
muhammad <allappsdata97@gmail.com>
2024-03-27 05:48:31 +0200

    first commit

--- src/pages/FetchProducts.jsx
+++ src/pages/FetchProducts.jsx
@@ -8,29 +8,21 @@ import { addproductApi } from '../redux/Products/AddProduct';
 export default function ShowProducts() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const { products } = useSelector((state) => state.fetchProductsSlice);
+  const state = useSelector((state) => state.fetchProductsSlice);
   const [formData, setFormData] = useState({
     ProductName: '',
     Price: '',
     Image: '',
   });
-  const [isRender, setIsRender] = useState(false);
-  const [currentPage, setCurrentPage] = useState(1);
-  const elementsPerPage = 12;
-  const indexOfLastElement = currentPage * elementsPerPage;
-  const indexOfFirstElement = indexOfLastElement - elementsPerPage;
-  const data = products?.products?.slice(indexOfFirstElement, indexOfLastElement);
-  const totalPages = Math.ceil(products?.products?.length / elementsPerPage);
-  const pagesArray = Array.from({ length: totalPages }, (_, index) => index + 1);
 
-  console.log(products)
+
 
   useEffect(() => {
     const fetchData = async () => {
       await dispatch(fetchProducts());
     };
     fetchData();
-  }, [dispatch, isRender]);
+  }, [dispatch]);
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
@@ -51,10 +43,16 @@ export default function ShowProducts() {
 
   const deleteProduct = async (_id) => {
     try {
-      await dispatch(deleteproductApi(_id));
-      setIsRender(!isRender);
-    } catch (error) {
+     const res= await dispatch(deleteproductApi(_id));
+    //  if(res.meta.requestStatus==='fulfilled')
+    //  {
+    //   continue;
+    //  }
+    } 
+    catch (error) 
+    {
       console.error('Error deleting product:', error.message);
+
     }
   };
 
@@ -62,28 +60,27 @@ export default function ShowProducts() {
   const addProduct = async (e) => {
     e.preventDefault();
     try {
-      const formDataToSend=new FormData();
-      formDataToSend.append('ProductName', formData.ProductName);
-      formDataToSend.append('Price', formData.Price);
-      formDataToSend.append('Image', formData.Image);
-      const res=await dispatch(addproductApi(formDataToSend));
-      setIsRender(!isRender);
+      const res=await dispatch(addproductApi(formData));
       console.log(res)
-      setFormData({
-        ProductName: '',
-        Price: '',
-        Image: '',
-      });
-    } catch (error) {
+      if(res.meta.requestStatus==='fulfilled')
+      {
+        setFormData({
+          ProductName: '',
+          Price: '',
+          Image: '',
+        });
+        show_products()
+      }
+    } catch (error) 
+    {
       console.error('Error adding product:', error);
     }
   };
 
-
-  return (
-    <>
-      <div className="products" style={{ height: '100vh' }}>
-        {data?.map((product) => (
+  const show_products=()=>{
+    return <>
+          <div className="products" style={{ height: '100vh' }}>
+        {state?.products?.data.map((product) => (
           <div className="card" style={{ width: '12rem', height: '16rem' }} key={product._id}>
             <div style={{ textAlign: 'right' }}>
               <span
@@ -101,108 +98,22 @@ export default function ShowProducts() {
               <p className="card-text">{product.Price}$</p>
             </div>
             <div className="center-btn">
-              <button type="button" className="btn btn-primary" onClick={() => navigate(`/product/${product._id}`)}>
+              <button type="button" className="btn btn-primary">
                 Buy
               </button>
             </div>
           </div>
         ))}
       </div>
+    </>
+  }
 
-      <div style={{ position: 'fixed', bottom: '10px', right: '10px' }}>
-        <button
-          type="button"
-          className="btn btn-primary"
-          data-bs-toggle="modal"
-          style={{ width: '50px', height: '50px', borderRadius: '25px' }}
-          data-bs-target="#exampleModal"
-          data-bs-whatever=""
-        >
-          +
-        </button>
-      </div>
 
-      <div className="modal fade" id="exampleModal" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div className="modal-dialog">
-          <div className="modal-content">
-            <div className="modal-header">
-              <h1 className="modal-title fs-5" id="exampleModalLabel">
-                Add product
-              </h1>
-              <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div className="modal-body" style={{ justifyContent: 'center', display: 'flex' }}>
-              <form onSubmit={addProduct} style={{ border: 'none' }}>
-                <div className="mb-3">
-                  <label htmlFor="recipient-name" className="col-form-label">
-                    ProductName
-                  </label>
-                  <input
-                    type="text"
-                    className="form-control"
-                    id="recipient-name"
-                    name="ProductName"
-                    value={formData.ProductName}
-                    onChange={handleInputChange}
-                    required
-                  />
-                </div>
-                <div className="mb-3">
-                  <label htmlFor="message-text" className="col-form-label">
-                    Price
-                  </label>
-                  <input
-                    type="number"
-                    value={formData.Price}
-                    className="form-control"
-                    id="recipient-name"
-                    name="Price"
-                    onChange={handleInputChange}
-                    required
-                  />
-                </div>
-                <div className="mb-3">
-                  <label htmlFor="message-text" className="col-form-label">
-                    Image
-                  </label>
-                  <input
-                    type="file"
-                    className="form-control"
-                    id="recipient-name"
-                    name="Image"
-                    accept="image/*"
-                    onChange={handleInputChange}
-                    required
-                  />
-                </div>
-                <div className="modal-footer">
-                  <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">
-                    Close
-                  </button>
-                  <button type="submit" className="btn btn-primary">
-                    Add
-                  </button>
-                </div>
-              </form>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <ul className="pagination justify-content-center" style={{ alignItems: 'center', marginTop: '50px' }}>
-        {pagesArray.map((page) => (
-          <li className="page-item" key={page}>
-            <button
-              className="page-link"
-              style={currentPage === page ? { fontWeight: 'bolder' } : {}}
-              onClick={() => setCurrentPage(page)}
-              disabled={currentPage === page}
-            >
-              {page}
-            </button>
-          </li>
-        ))}
-      </ul>
+  return (
+    <>
+    {
+      show_products()
+    }
     </>
   );
 }
